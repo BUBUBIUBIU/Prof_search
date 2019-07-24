17c49eae861c1b6a245a499c656c2c166dc60c9a
Wei Lin <weilin@Weis-MacBook-Pro.local>
2019-07-24 15:13:36 +1000

    wiwi

--- src/ui/Login/LoginPage.jsx
+++ src/ui/Login/LoginPage.jsx
@@ -135,7 +135,7 @@ class LoginPage extends Component {
                 </div>
             </Paper>
 
-            <Paper className ={classes.paper} style ={{padding:"20px 30px"}}>
+            <Paper className ={classes.paper} style ={{padding:"50px 30px"}}>
                 <BootstrapStyleSearchBox
                     label = "Email Adress"
                     placeHolder = "xxx@gmail.com"
@@ -146,7 +146,7 @@ class LoginPage extends Component {
                 {/* todo */}
                 <BootstrapStyleSearchBox    
                     label = "Password"
-                    placeHolder = "Please enter yout password"
+                    placeHolder = "Please enter your password"
                     compusory = {true}
                     onChangeInput = {this.handleChange("password")}
                     />
--- src/ui/personalProfilePage/Publication.jsx
+++ src/ui/personalProfilePage/Publication.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
-import { Paper,Typography,Collapse, Button, withStyles,ToolBar } from '@material-ui/core';
+import { Paper,Typography,Collapse, Button, withStyles,ToolBar, Modal,FormControl,NativeSelect,InputBase  } from '@material-ui/core';
 import Icon from '@material-ui/core/Icon';
-import { Plus } from 'mdi-material-ui'
+import { Plus, Close } from 'mdi-material-ui';
+
+import BootstrapStyleSearchBox from '../reusableComponents/BootstrapStyleSearchBox'
 
 const styles = theme => ({
     paper:{
@@ -14,6 +16,16 @@ const styles = theme => ({
         padding:"20px 0px 0 30px",
         boxShadow: "0 2px 4px 0 rgba(215, 215, 215, 0.5)",
         // width:"100%"
+      },
+      modal: {
+        position: 'absolute',
+        width: "800px",
+        backgroundColor: theme.palette.common.white,
+        boxShadow: theme.shadows[5],
+        outline: 'none',
+        top:"20px"  ,
+        left:"300px",
+        borderRadius: "4px"
       }
   });
 
@@ -25,6 +37,14 @@ class Publication extends Component {
 
         };
     }
+
+    handleOpen = () => {
+        this.setState({ open: true });
+      };
+    
+      handleClose = () => {
+        this.setState({ open: false });
+      };
 
     handleOnclick = () =>{
         if (this.state.expand == false){
@@ -46,17 +66,52 @@ class Publication extends Component {
                 </div>
                 </Typography >    
 
-                <Button color="primary" style= {{marginRight: "20px",float: "right",verticalAlign:"middle"}} size="small" onClick = {this.handleOnclick}>
+                <Button color="primary" style= {{marginRight: "20px",float: "right",verticalAlign:"middle"}} size="small" onClick = {this.handleOpen}>
                 <Plus/> Add Publication
                 </Button>   
                 </div>
             </Paper>
 
-            <Collapse  in = {this.state.expand} timeout="auto" unmountOnExit>
+{/*             <Collapse  in = {this.state.expand} timeout="auto" unmountOnExit>
                 <Paper>
                     something
                 </Paper>
-            </Collapse>
+            </Collapse> */}
+
+ <Modal
+            aria-labelledby="simple-modal-title"
+            aria-describedby="simple-modal-description"
+            open={this.state.open}
+            onClose={this.handleClose}
+            >
+            <div className = {classes.modal}>
+            <Paper className ={classes.paper} style ={{padding:"20px 30px 0px 30px", marginBottom:"3px",height:"40px"}} >
+                <div>
+                <Typography variant ="h1">
+                <div style ={{verticalAlign:"middle",height:"100%", float: "left"}}>
+                 Update Publication
+                </div>
+                <Button style= {{float: "right",verticalAlign:"middle", color:"#000000"}} size="small" onClick = {this.handleClose}>
+                <Close/>
+                </Button>
+                </Typography >       
+                </div>
+            </Paper>
+
+            <Paper className ={classes.paper} style ={{padding:"20px 30px"}}>
+                <BootstrapStyleSearchBox
+                    label = "Brief Description"
+                    compusory = {true}
+                    />
+
+                <BootstrapStyleSearchBox    
+                    label = "URL"
+                    compusory = {true}
+                    />
+                
+            </Paper>
+            </div>
+            </Modal> 
             </div>
         )
     }
